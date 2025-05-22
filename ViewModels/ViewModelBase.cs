2ae58f2c0525ac2d27b116dbe5c0ae4ebfb77561
Marat <maratf76@gmail.com>
2025-05-22 16:41:30 +0500

    Добавьте файлы проекта.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaTuring.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaTuring.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
